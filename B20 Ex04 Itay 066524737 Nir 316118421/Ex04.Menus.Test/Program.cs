575184c4afe39cbf020df8c06e3b24833e5265d2
Itay Cohen <itaycs2407@gmail.com>
2020-06-08 23:05:44 +0300

    finish the method to invoke

--- B20 Ex04 Itay 066524737 Nir 316118421/Ex04.Menus.Test/Program.cs
+++ B20 Ex04 Itay 066524737 Nir 316118421/Ex04.Menus.Test/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ex04.Menus.Test
@@ -10,6 +12,47 @@ namespace Ex04.Menus.Test
     {
         static void Main(string[] args)
         {
+            showTime();
+            showDate();
+            showVersion();
+            countCapitals();
+            Console.ReadLine();
+        }
+
+        private static void countCapitals()
+        {
+            int i, capitalCounter = 0;
+            char currentLetter;
+            Console.WriteLine("Please write a sentence :");
+            string sentenceFromUser = Console.ReadLine();
+            for (i = 0; i < sentenceFromUser.Length; i++)
+            {
+                currentLetter = sentenceFromUser[i];
+                if (char.IsUpper(currentLetter))
+                {
+                    capitalCounter++;
+                }
+            }
+            Console.WriteLine(@"There are {0} capital letters in the sentence you entered.", capitalCounter);
+        }
+
+        private static void showVersion()
+        {
+           Console.WriteLine("Version: 20.2.4.30620");
+        }
+
+        private static void showTime()
+        {
+            DateTime time = new DateTime();
+            time = DateTime.Now;
+            Console.WriteLine(@"Current time is : {0}:{1}", time.Hour, time.Minute);
+        }
+
+        private static void showDate()
+        {
+            DateTime time = new DateTime();
+            time = DateTime.Now;
+            Console.WriteLine(@"Current date is : {0}/{1}/{2}", time.Day, time.Month, time.Year);
         }
     }
 }
